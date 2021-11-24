0f3fbbbaff27dfef2e213d4073df9b41f324c8d7
JejeB <jeremy.bonnefoi@isen.yncrea.fr>
2021-11-24 16:59:01 +0100

    change the data base by our data base

--- mib/models/user.py
+++ mib/models/user.py
@@ -1,4 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
+import bcrypt
 
 from mib import db
 
@@ -19,46 +21,49 @@ class User(db.Model):
 
     # All fields of user
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.Unicode(128), nullable=False, unique=True)
-    first_name = db.Column(db.Unicode(128), nullable=False, unique=False)
-    last_name = db.Column(db.Unicode(128), nullable=False, unique=False)
-    password = db.Column(db.Unicode(128))
-    birthdate = db.Column(db.Date())
-    phone = db.Column(db.Unicode(128), nullable=False, unique=True)
-    is_active = db.Column(db.Boolean, default=True)
+    email = db.Column(db.Unicode(128), nullable=False)
+    firstname = db.Column(db.Unicode(128))
+    lastname = db.Column(db.Unicode(128))
+    password = db.Column(db.LargeBinary(128)) # To avoid having warining. We store binary datas for the password (the result of bcrypt.hashpw)
+    date_of_birth = db.Column(db.DateTime)
+    is_active = db.Column(db.Boolean, default=True) # To know if a user is active, in the sense that its account is not deleted
     is_admin = db.Column(db.Boolean, default=False)
-    authenticated = db.Column(db.Boolean, default=True)
     is_anonymous = False
+    filter_isactive = db.Column(db.Boolean, default=False) #content filter for user
+    n_report = db.Column(db.Integer, default = 0) #number of report that the user received 
+    ban_expired_date = db.Column(db.DateTime, default = None) #data a cui finisce il ban dell'utente
+    lottery_ticket_number = db.Column(db.Integer, default = -1) #lottery ticker number 0-99
+    lottery_points = db.Column(db.Integer, default = 0) #points gained with the monthly lottery
+
+    black_list = relationship('User',
+    secondary=blacklist,
+    primaryjoin=id==blacklist.c.user_id,
+    secondaryjoin=id==blacklist.c.black_id,
+    backref="user_id",
+    lazy = 'dynamic')
 
     def __init__(self, *args, **kw):
         super(User, self).__init__(*args, **kw)
-        self.authenticated = False
+        self._authenticated = False
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        
+    def set_dateOfBirth(self, val):
+        self.date_of_birth = val
 
-    def set_email(self, email):
-        self.email = email
-
-    def set_first_name(self, name):
-        self.first_name = name
-
-    def set_last_name(self, name):
-        self.last_name = name
-
+    @property
     def is_authenticated(self):
-        return self.authenticated
-
-    def set_birthday(self, birthdate):
-        self.birthdate = birthdate
-
-    def set_phone(self, phone):
-        self.phone = phone
+        return self._authenticated
 
     def authenticate(self, password):
-        checked = check_password_hash(self.password, password)
-        self.authenticated = checked
-        return self.authenticated
+        #checked = check_password_hash(self.password, password) OLD
+        checked = bcrypt.checkpw(password.encode('utf-8'), self.password) #check password hash and salt
+        self._authenticated = checked
+        return self._authenticated
+
+    def get_id(self):
+        return self.id
 
     def serialize(self):
         return dict([(k, self.__getattribute__(k)) for k in self.SERIALIZE_LIST])
